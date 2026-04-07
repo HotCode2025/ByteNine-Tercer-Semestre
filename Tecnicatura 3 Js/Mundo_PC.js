c4c0ee1a6654705cb5161688b8896e08bf2085a6
AgusSantarena <santarenaagustin69@gmail.com>
2026-04-06 23:30:17 -0300

    Clase DispositivoEntrada

---
+++ Tecnicatura 3 Js/Mundo_PC.js
@@ -0,0 +1,18 @@
+class DispositivoEntrada{
+
+    get tipoEntrada(){
+        return this._tipoEntrada;
+    }
+
+    set tipoEntrada(tipoEntrada){
+        this._tipoEntrada = tipoEntrada;
+    }
+
+    get marca(){
+        return this._marca;
+    }
+
+    set marca(marca){
+        this._marca = marca;
+    }
+}
