34a6900243baae1c79b3dd028a3a68eea87f9ee7
santy171004 <santinmartin773@gmail.com>
2026-04-08 11:40:31 -0300

    1.3 Procesar clases de exception más específicas

--- Tecnicatura3Py/Excepciones/manejo_excepciones.py
+++ Tecnicatura3Py/Excepciones/manejo_excepciones.py
@@ -4,8 +4,12 @@ b = 0
 
 try:
     resultado = a/b  # modificamos
+except  TypeError as e:
+    print(f'TypeError - Ocurrió un error: {type(e)}')
 except  ZeroDivisionError as e:
-    print(f'Ocurrió un error: {e}')
+    print(f'ZeroDivisionError - Ocurrió un error: {type(e)}')
+except  Exception as e:
+    print(f'Exception - Ocurrió un error: {type(e)}')
 
 print(f'el resultado es: {resultado}')
 print(f'seguimos...')
