74d741c7af6fb6e014f92f3b4f65a436b5474601
GabrielSantarena <gabrielsantarena69@gmail.com>
2026-04-15 22:32:43 -0300

    Tarea Grupal
    Co-Authored-By: AgusSantarena <215079354+AgusSantarena@users.noreply.github.com>
    Co-Authored-By: Valeria  Lardin <valelar81@gmail.com>
    Co-Authored-By: Gustavo Ariel Rodriguez <garodrifornes79@gmail.com>
    Co-Authored-By: santy171004 <215538451+santy171004@users.noreply.github.com>
    Co-Authored-By: Nahuel Spikerman <215084796+nawe2@users.noreply.github.com>
    Co-Authored-By: Maximiliano <215079477+MaxiMorales96@users.noreply.github.com>
    
    #27

--- Tecnicatura3J/Leccion02/BloquesInicializacion/src/domain/Persona.java
+++ Tecnicatura3J/Leccion02/BloquesInicializacion/src/domain/Persona.java
@@ -10,10 +10,32 @@ public class Persona {
         // idPersona = 10; No es estático un atributo, por esto tenemos un error
     }
     
+    /* Remplazo bloque static
+    private static int contadorPersonas = inicializarContador();
+
+    private static int inicializarContador() {
+        System.out.println("Ejecución del bloque estático");
+        return 1; // En lugar de ++contadorPersonas, devolvemos el valor inicial
+    }
+    */
+    
     { //Bloque de inicialización NO estático (contexto dinámico)
         System.out.println("Ejecución del bloque NO estático");
         this.idPersona = Persona.contadorPersonas++; //Incrementamos el atributo
     }
+    
+    /* Remplazo bloque no static
+    private int idPersona;
+
+        public Persona() {
+            this(0); 
+        }
+
+        public Persona(int parametro) {
+            System.out.println("Ejecución del bloque no estatico");
+            this.idPersona = parametro;
+        }        
+    */
     
     //Los bloques de inicialización se ejecutan antes del constructor
     
