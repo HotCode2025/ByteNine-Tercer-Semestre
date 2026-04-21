c79107dac23f62c958408a0996ae013d847742b9
Gustavo Ariel Rodriguez <garodrifornes79@gmail.com>
2026-04-20 21:18:46 -0300

    Crear Catalogo Peliculas

---
+++ Tecnicatura3Py/Leccion02/catalogo_peliculas/servicio/catalogo_peliculas.py
@@ -0,0 +1,52 @@
+# Importación
+from dominio.pelicula import Pelicula
+
+# Defiinición de la clase
+class CatalogoPeliculas:
+    """Realizar las operaciones sobre el archivo de peliculas.txt."""
+
+    # Atributo de clase
+    ruta_archivo: str ="peliculas.txt"
+
+    # Método Agregar_peliculas()
+    @staticmethod
+    def agregar_pelicula(pelicula: Pelicula):
+
+        with open(CatalogoPeliculas.ruta_archivo, "a", encoding="utf-8") as archivo:
+            archivo.write(str(pelicula) + "\n")
+
+        print(f' Película "{pelicula}" agregada correctamente.')
+
+    # Método listar_peliculas()
+    @staticmethd
+    def listar_peliculas():
+        try:
+            with open(CatalogoPeliculas.ruta_archivo, "r", encoding="utf-8") as archivo:
+                lineas =[l.strip() for l in archivo.readlines() if l.strip()]
+
+            if not lineas:
+                print(" No hay películas en el catálogo. ")
+            else:
+                # '<5' alinea el número a la izquierda en 5 caracteres
+                print(f"\n {'N°':<5} {'Título'}")
+                print(" " + "-" * 35)
+
+                for i, titulo in enumerate(lineas, 1):
+                    print(f" {i:<5} {titulo}")
+
+        except FileNotFoundError:
+            # el archivo no existe: se informa sin interrrupir el programa
+            print(" El catálogo aún no existe. Agregue una película primero.")
+
+    # Método elimimar()
+    @staticmethod
+    def eliminar():
+        import os
+        try:
+            os.remove(CatalogoPeliculas.ruta_archivo)
+            print(" Archivo de películas eliminado correctamente.")
+
+        except FileNotFoundError:
+            print(" el archivo no existe, no hay nada que eliminar. ")
+
+
