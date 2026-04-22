e214e5e11b329cee68189955cba7702df6ee640c
GabrielSantarena <gabrielsantarena69@gmail.com>
2026-04-22 19:55:13 -0300

    1.3 Modificadores de acceso public
    #35

---
+++ Tecnicatura3J/Leccion3/ModificadoresAcceso/src/paquete1/Clase1.java
@@ -0,0 +1,15 @@
+
+package paquete1;
+
+
+public class Clase1 {
+    public String atrubutoPublic = "Valor atributo public";
+    
+    public Clase1(){
+        System.out.println("Constructor public");
+    }
+    
+    public void metodoPublico(){
+        System.out.println("Metodo public");
+    }
+}
