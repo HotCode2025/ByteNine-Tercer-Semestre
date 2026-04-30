bd959e3b4587fcfe1954933a6e2f2913eb733113
GabrielSantarena <gabrielsantarena69@gmail.com>
2026-04-29 22:07:11 -0300

    1.6 Conexión hacia la base de datos en Python con el método fetchall()
    #43

---
+++ Tecnicatura3Py/Leccion04/BD/prueba_bd.py
@@ -0,0 +1,14 @@
+import psycopg2 #Esto es para poder conectarnos a Postgre
+
+conexion = psycopg2.connect(
+    user="postgres",
+    password="46326123Gabi",
+    host="127.0.0.1",
+    port="5432",
+    database="test_db"
+)
+
+cursor = conexion.cursor()
+sentencia = "SELECT * FROM persona"
+cursor.execute(sentencia) #De esta manera ejecutamso la sentencia
+registro = cursor.fetchall() #Recuperamos todos los registros que seran una lista
