d2ca233751c7175ea81aafcccd6f250ebc0aaab5
GabrielSantarena <gabrielsantarena69@gmail.com>
2026-05-04 16:29:24 -0300

    Actualizar prueba_bd.py

--- Tecnicatura3Py/Leccion04/BD/prueba_bd.py
+++ Tecnicatura3Py/Leccion04/BD/prueba_bd.py
@@ -2,7 +2,7 @@ import psycopg2 #Esto es para poder conectarnos a Postgre
 
 conexion = psycopg2.connect(
     user="postgres",
-    password="46326123Gabi",
+    password="",
     host="127.0.0.1",
     port="5432",
     database="test_db"
