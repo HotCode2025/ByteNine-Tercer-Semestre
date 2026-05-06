26a3fef109b18dd3c4b1f7ca0793fa02ac67a9c9
GabrielSantarena <gabrielsantarena69@gmail.com>
2026-05-06 16:06:48 -0300

    5.6 Actualizar o modificar un registro
    #57

--- Tecnicatura3Py/Leccion04/BD/v_i_registros.py
+++ Tecnicatura3Py/Leccion04/BD/v_i_registros.py
@@ -1,22 +1,17 @@
 import psycopg2 #Esto es para poder conectarnos a Postgre
 
-conexion = psycopg2.connect(user="postgres", password="46326123Gabi", host="127.0.0.1", port="5432", database="test_db")
+conexion = psycopg2.connect(user="admin", password="admin", host="127.0.0.1", port="5432", database="test_db")
 try:
     with conexion:
         with conexion.cursor() as cursor:
-            sentencia = 'INSERT INTO persona (nombre, apellido, email)VALUES (%s, %s, %s )' # Placeholder
-            valores = (
-                ('Carlos', 'Lara', 'clara@mail.com'),
-                ("Marcos", "Canto", "mcanto@gmail.com"),
-                ("Marcelo", "Cuenca", "cuencaM@gmail.com")
-            ) # Esto es una Tupla de Tuplas.
-            cursor.executemany(sentencia, valores) #De esta manera ejecutamso la sentencia
-            #conexion.commit()# No lo utilizamos para guardar cambios en la base de datos porque el whith lo hace automaticamente
-            registros_insertados = cursor.rowcount
-            print(f'Los registros insertadaos son: {registros_insertados}')
+            sentencia = "UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s"
+            valores = ("Juan Carlos", "Roldan", "rcarlos@mail.com", 1) # Esto es una Tupla de Tuplas.
+            cursor.execute(sentencia, valores) #De esta manera ejecutamso la sentencia
+            registros_actualizados = cursor.rowcount
+            print(f'Los registros actualizados son: {registros_actualizados}')
 
 
 except Exception as e:
-    print(f'Ocurrió un error: {e}') 
+    print(f'Ocurrió un error: {e}')
 finally:
     conexion.close()
