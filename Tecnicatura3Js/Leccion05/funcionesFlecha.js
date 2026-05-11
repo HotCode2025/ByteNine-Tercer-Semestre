5b35458b7e4d1b71ceb04ea6ba5b11903decac30
Maximiliano <maxiarielmorales712@gmail.com>
2026-05-11 16:46:35 -0300

    5.2 Avanzamos en diferentes ejemplos Video 04

--- Tecnicatura3Js/Leccion05/funcionesFlecha.js
+++ Tecnicatura3Js/Leccion05/funcionesFlecha.js
@@ -23,7 +23,7 @@ miFuncionFlecha();
 // lo hacemos en una sola linea
 const saludar = () => console.log('Saludos a todos desde esta función flecha');
 
-console.log(saludar);
+saludar();
 
 // Otro ejemplo
 const saludar2 = ()  => {
@@ -31,12 +31,12 @@ const saludar2 = ()  => {
 
 }
 
-console.log(saludar2);
+console.log(saludar2());
 
 // Simplificamos la funcion anterior
 const saludar3 = () => ' Saludos desde la función flecha tres';
 
-console.log(saludar3)
+console.log(saludar3());
 
 //Continuamos con otro ejemplo
 const regresaObjeto = () => ({nombre: 'Juan', apellido: 'Lara'});
@@ -59,3 +59,11 @@ funcionParametrosClasica('Saludos desde la función clasica');
 const funcionConParametros = mensaje => console.log( mensaje );
 
 funcionConParametros('Otra forma de trabajar con función flecha');
+
+//AHora vemos funciones flecha con varios parámetros
+//Podemos abrir la funcion y tener mas cosas dentro de ella
+const funcionConParametros2 = (op1, op2) => {
+    let resultado = op1 + op2;
+    return resultado;
+}
+console.log(funcionConParametros2(3, 5));
