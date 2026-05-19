0ec7aa84b3efd23d0de06c7adfa8e0723970df68
GabrielSantarena <gabrielsantarena69@gmail.com>
2026-05-18 22:22:43 -0300

    8.3 Creación de la Clase Conexion: Video 4
    #94

--- Tecnicatura3Py/Leccion07/capa_datos_personas/conexion.py
+++ Tecnicatura3Py/Leccion07/capa_datos_personas/conexion.py
@@ -1,3 +1,8 @@
+import psycopg2
+# psycopg2 as db #Otra manera de importar el psycopg2
+from logger_base import log
+import sys
+
 class Conexion:
     _DATABASE = 'test_db'
     _USERNAME = 'postgres'
@@ -6,3 +11,21 @@ class Conexion:
     _HOST = '127.0.0.1'
     _conexion = None
     _cursor = None
+
+    @classmethod
+
+    def obtener_conexion(cls):
+        if cls._conexion is None:
+            try:
+                cls._conexion = bd.connect(hot=cls._HOST,
+                                           user=cls._USERNAME,
+                                           password=cls._PASSWORD,
+                                           port=cls._DB_PORT,
+                                           database=cls._DATABASE)
+                log.debug(f"Conexion Exitosa: {cls._conexion}")
+                return cls._conexion
+            except Exception as e:
+                log.error(f"Ocurrio un error: {e}")
+                sys.exit()
+            else:
+                return cls._conexion
