7c7d7b4446b57c57a15503deb011f926f2a9da9c
GabrielSantarena <gabrielsantarena69@gmail.com>
2026-05-29 15:31:00 -0300

    8.4 Creamos nuestra propia excepción - Video 8
    #113

---
+++ Tecnicatura3J/Leccion08/ManejoExepciones1/src/aritmetica/Aritmetica.java
@@ -0,0 +1,14 @@
+
+package aritmetica;
+
+import javax.management.openmbean.OpenDataException;
+
+
+public class Aritmetica {
+    public static int division(int numerador, int denominador) throws OpenDataException{
+        if(denominador == 0){
+            throw new OpenDataException("Division entre cero");
+        }
+        return numerador / denominador;
+    }
+}
