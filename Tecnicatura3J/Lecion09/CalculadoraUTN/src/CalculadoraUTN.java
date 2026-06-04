0edd97d934a587fd74034f1212ff5555a8181118
AgusSantarena <santarenaagustin69@gmail.com>
2026-06-03 23:07:13 -0300

    9.2 Definiendo las variables
    #126

---
+++ Tecnicatura3J/Lecion09/CalculadoraUTN/src/CalculadoraUTN.java
@@ -0,0 +1,11 @@
+
+public class CalculadoraUTN {
+
+    static void main(String[] args) {
+        System.out.println("******* Aplicación Calculadora *******");
+        var operando1 = 10;
+        var operando2 = 20;
+        var resultado = operando1 + operando2;
+        System.out.println("resultado = " + resultado);
+    }
+}
