df60bcff19da9b8c56d16dd00666e61e287a0697
AgusSantarena <santarenaagustin69@gmail.com>
2026-06-03 23:11:27 -0300

    9.3 Ponemos la entrada de información
    #127

--- Tecnicatura3J/Lecion09/CalculadoraUTN/src/CalculadoraUTN.java
+++ Tecnicatura3J/Lecion09/CalculadoraUTN/src/CalculadoraUTN.java
@@ -1,11 +1,16 @@
+import java.util.Scanner;
 
 public class CalculadoraUTN {
 
     static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
         System.out.println("******* Aplicación Calculadora *******");
-        var operando1 = 10;
-        var operando2 = 20;
+        System.out.print("Digite el valor para operando1: ");
+        var operando1 = Integer.parseInt(entrada.nextLine());
+        System.out.print("Digite el valor para operando2: ");
+        var operando2 = Integer.parseInt(entrada.nextLine());
         var resultado = operando1 + operando2;
         System.out.println("resultado = " + resultado);
+
     }
 }
