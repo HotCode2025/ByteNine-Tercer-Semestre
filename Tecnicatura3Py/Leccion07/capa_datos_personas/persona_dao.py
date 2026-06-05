03948531f322ec0c73dbdcffd8f4e8837a0eff94
GabrielSantarena <gabrielsantarena69@gmail.com>
2026-06-04 22:13:53 -0300

    9.5 Método Eliminar
    #122

--- Tecnicatura3Py/Leccion07/capa_datos_personas/persona_dao.py
+++ Tecnicatura3Py/Leccion07/capa_datos_personas/persona_dao.py
@@ -47,13 +47,26 @@ class PersonaDAO:
                 cursor.execute(cls._ACTUALIZAR, valores)
                 log.debug(f'Persona actualizada: {persona}')
                 return cursor.rowcount
+    @classmethod
+    def eliminar(cls, persona):
+        with Conexion.obtener_conexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.id_persona,)
+                cursor.execute(cls._ELIMINAR, valores)
+                log.debug(f"Los objetos eliminados son {persona}")
+                return cursor.rowcount
 
 
 if __name__ == '__main__':
+    # Eliminar un registro
+    persona1 = Persona(id_persona=8)
+    personas_eliminadas = PersonaDAO.eliminar(persona1)
+    log.debug(f"Personas eliminadas: {personas_eliminadas}")
+
     # Actualizar un registro
-    persona1 = Persona(1, 'Juan Jose', 'Pena', 'jjpena@gmail.com' )
-    personas_actualizadas = PersonaDAO.actualizar(persona1)
-    log.debug(f'Personas actualizadas: {personas_actualizadas}')
+    # persona1 = Persona(1, 'Juan Jose', 'Pena', 'jjpena@gmail.com' )
+    # personas_actualizadas = PersonaDAO.actualizar(persona1)
+    # log.debug(f'Personas actualizadas: {personas_actualizadas}')
 
     # Insertar un registro
     # persona1 = Persona(nombre='Omero', apellido='Ramos', email='omeror@mail.com')
