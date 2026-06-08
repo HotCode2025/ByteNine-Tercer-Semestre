1bcf585e1218aa23658f7dc087304eee575e6a6d
Nahuel Spikerman <nahuelspikerman@gmail.com>
2026-06-08 20:54:19 -0300

    9.4 Cargamos el menú

--- Tecnicatura3J/Lecion09/CalculadoraUTN/src/CalculadoraUTN.java
+++ Tecnicatura3J/Lecion09/CalculadoraUTN/src/CalculadoraUTN.java
@@ -1,16 +1,25 @@
 import java.util.Scanner;
 
 public class CalculadoraUTN {
-
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("******* AplicaciÃ³n Calculadora *******");
-        System.out.print("Digite el valor para operando1: ");
-        var operando1 = Integer.parseInt(entrada.nextLine());
-        System.out.print("Digite el valor para operando2: ");
-        var operando2 = Integer.parseInt(entrada.nextLine());
-        var resultado = operando1 + operando2;
-        System.out.println("resultado = " + resultado);
-
+        System.out.println("******* Aplicacion Calculadora *******");
+        //Mostramos el menú
+        System.out.println("""
+                1. Suma
+                2. Resta
+                3. Multiplicacion
+                4. Division
+                5. Salir
+                """);
+        System.out.print("Operacion a realizar? ");
+        var operacion = Integer.parseInt(entrada.nextLine());
+        
+        if(operacion >= 1 && operacion <= 4){
+            System.out.print("Digite el valor para operando1: ");
+            var operando1 = Integer.parseInt(entrada.nextLine());
+            System.out.print("Digite el valor para operando2: ");
+            var operando2 = Integer.parseInt(entrada.nextLine());
+        }
     }
 }
