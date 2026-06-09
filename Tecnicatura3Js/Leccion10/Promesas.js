4605d0e0249f84922084069dbc2d51e04cd1f063
santy171004 <santinmartin773@gmail.com>
2026-06-08 23:01:19 -0300

    10.3 Promesas: La práctica con la función .catch
    #142

--- Tecnicatura3Js/Leccion10/Promesas.js
+++ Tecnicatura3Js/Leccion10/Promesas.js
@@ -7,7 +7,11 @@ let miPromesa = new Promise((resolver, rechazar) => {
     }
 });
 
-miPromesa.then(
-    valor => console.log(valor),
-    error => console.log(error)
-);
+//miPromesa.then(
+//    valor => console.log(valor),
+//    error => console.log(error)
+//);
+
+miPromesa
+         .then(valor => console.log(valor))
+         .catch(error => console.log(error));
