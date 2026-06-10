10a982a02fb83730837a4570fed75fced0968744
Gustavo Ariel Rodriguez <garodrifornes79@gmail.com>
2026-06-10 20:24:25 -0300

    10.5 Palabra async con promesas
    #146

--- Tecnicatura3Js/Leccion10/Promesas.js
+++ Tecnicatura3Js/Leccion10/Promesas.js
@@ -17,10 +17,17 @@ let miPromesa = new Promise((resolver, rechazar) => {
 //         .catch(error => console.log(error));
 
 let promesa = new Promise( (Resolver) => {
-    console.log('Inicio promesa');
+    //console.log('Inicio promesa');
     setTimeout( () => Resolver('Saludos desde promesa, callback, función flecha y setTimeout'), 3000);
-    console.log('Final promesa');
+    //console.log('Final promesa');
 })
 
 //El llamado a la promesa
-promesa.then( valor => console.log(valor));
+//promesa.then( valor => console.log(valor));
+
+// async indica que la función regresa una promesa
+async function miFuncionConPromesa(){
+    return 'Saludos con promesa y async';
+}
+
+miFuncionConPromesa().then(valor => console.log(valor));
